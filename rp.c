2dfa79b84247620c7f6381bbd88bde3e5a4f7d20
Albin Sundqvist <alsu8190@fries.it.uu.se>
2014-10-09 13:19:14 +0200

    Git skit

--- rp.c
+++ rp.c
@@ -3,6 +3,8 @@
 #include <string.h>
 
 struct edge;
+
+
 typedef struct stop{
   char *name;
   struct edge *route;
@@ -10,128 +12,195 @@ typedef struct stop{
 } *Stop;
 
 typedef struct edge{
-  int *line;
+  char *line;
   struct stop *start;
   struct stop *stop;
-  int *time;
+  char *time;
   struct edge *next;
 }*Edge;
+
+//Git skit
+
+char reformatLine(char line[128]){
+  for(int i=0;i<=128;i++){
+    if( line[i]==","){
+      line[i]="\0";
+      line[i++];
+    }
+  }
+}
 
 void readline(char *dest, int n, FILE *source){
   fgets(dest, n, source);
   int len = strlen(dest);
-  if(dest[len-1] == '\n'|| dest[len-1] == ',')
+  if(/*dest[len-1] == '\n' || */dest[len-1] == ',')
     dest[len-1] = '\0';
 }
 
-int Inlist(Stop List, char key){
+int Inlist(Stop List, char key[128]){
+
   if(List){
-    if(List->key==name)
+    if(strcmp(key,List->name)==0){
       return 1;
-    else
+    }
+    else{
       Inlist(List->next,key);
+    }
   }
-  else 
-    return 0;
+  return 0;
+  
 }
+
+void InsertEdge(Edge *Edges, Edge *newEdge){
+  if (!(*Edges)){
+    *Edges=*newEdge;
+  }
+  else{
+    (*newEdge)->next=*Edges;
+    *Edges=*newEdge;
+  }
+}
+
+/*char delim[2] = ",";
+strtok(str, delim)
+
+if (string[0] == 32)
+  string = string + 1
+*/
+
+void printStops(Stop Stops){
+  while(Stops){
+    printf("%s\n", Stops->name);
+    Stops=Stops->next;
+  }
+}
+
 
 void InsertStop(Stop *Stops, Stop *newStop){
   if(!(*Stops)){
     *Stops=*newStop;
   }
   else{
-    if(!Inlist(Stops,newStop->name)){
+    if(!Inlist(*Stops,(*newStop)->name)){
       (*newStop)->next=*Stops;
       *Stops=*newStop;
     }
-
     else
-      puts("\nIndex already loaded.\n")
-
-
-	}
+      puts("\nIndex already loaded.\n");
+  }    
 }
 
-void createLists(char *filename){
+/*void createLists(char *filename){
   char buffer[128];
   FILE *buschart = fopen(filename, "r");
   Stop Stops=NULL;
   Edge Edges =NULL;
+  char newstartname[128], newstopname[128];
   while(!(feof(buschart))){
     Stop newStop=malloc(sizeof(struct stop));
     Edge newEdge=malloc(sizeof(struct edge));
 
     readline(buffer, 128, buschart);
     newEdge->line=malloc(strlen(buffer)+1);
-    strcpy(newEdge->route->line,buffer);
+    strcpy(newEdge->line,buffer);
 
     readline(buffer, 128, buschart);
     newStop->name = malloc(strlen(buffer)+1);
     newEdge->start = malloc(strlen(buffer)+1);
-    strcpy(newEdge->start, buffer);
+    strcpy(newstartname, buffer);
+    newEdge->start->name=newstartname;
     strcpy(newStop->name, buffer);
 
     readline(buffer, 128, buschart);
     newEdge->stop = malloc(strlen(buffer)+1);
-    strcpy(newEdge->stop, buffer);
-    
+    strcpy(newstopname, buffer);
+    newEdge->stop->name=newstopname;
+
+
     readline(buffer, 128, buschart);
     newEdge->time=malloc(strlen(buffer)+1);
     strcpy(newEdge->time,buffer);
-  
+
+
     newEdge->next=NULL;
+    newEdge->start->route=NULL;
+    newEdge->start->next=NULL;
+    newEdge->stop->route=NULL;
+    newEdge->stop->next=NULL;
     newStop->next=NULL;
     newStop->route=NULL;
     InsertStop(&Stops, &newStop);
-    InsertEdge(&Edges, &newEdge)
-      }
+    InsertEdge(&Edges, &newEdge);
+  }
 }
-
-int main(){
-  return 0;
-}
-
-
-
-
-
-
-/*
-  void Insert(Map *map, Map *newStop){
-  if(!(*map)){
-  *map=*newStop;
+*/
+int main(int argc, char *argv[]){
+  if (argc < 2){
+    puts("Usage: rp [FILE]");
+    return -1;  
   }
-  else{
-    
-  }
-  }
-
-  Map createMap(char *filename){
+  char *filename = argv[1];
+  //  createLists(filename);
   char buffer[128];
   FILE *buschart = fopen(filename, "r");
-  Map map=NULL;
+  Stop Stops=NULL;
+  Edge Edges =NULL;
+  char newstartname[128], newstopname[128];
   while(!(feof(buschart))){
-  Map newStop = malloc(sizeof(struct map));
-  //Voyage newRoute = malloc(sizeof(struct voyage))
-  readline(buffer, 128, buschart);
-  newStop->route->line=malloc(strlen(buffer)+1);
-  strcpy(newStop->route->line,buffer);
+    Stop newStop=malloc(sizeof(struct stop));
+    Edge newEdge=malloc(sizeof(struct edge));
 
-  readline(buffer, 128, buschart);
-  newStop->stationname = malloc(strlen(buffer)+1);
-  newStop->route->connection1 = malloc(strlen(buffer)+1);
-  strcpy(newStop->route->connection1, buffer);
-  strcpy(newStop->stationname, buffer);
+    readline(buffer, 128, buschart);
+    newEdge->line=malloc(strlen(buffer)+1);
+    strcpy(newEdge->line,buffer);
 
-  readline(buffer, 128, buschart);
-  newStop->route->connection2 = malloc(strlen(buffer)+1);
-  strcpy(newStop->route->connection2, buffer);
-    
-  readline(buffer, 1128, buschart);
-  newStop->route->time=malloc(strlen(buffer)+1);
-  strcpy(newStop->route->time,buffer)
+    readline(buffer, 128, buschart);
+    newStop->name = malloc(strlen(buffer)+1);
+    newEdge->start = malloc(strlen(buffer)+1);
+    strcpy(newstartname, buffer);
+    newEdge->start->name=newstartname;
+    strcpy(newStop->name, buffer);
 
+    readline(buffer, 128, buschart);
+    newEdge->stop = malloc(strlen(buffer)+1);
+    strcpy(newstopname, buffer);
+    newEdge->stop->name=newstopname;
+
+
+    readline(buffer, 128, buschart);
+    newEdge->time=malloc(strlen(buffer)+1);
+    strcpy(newEdge->time,buffer);
+
+
+    newEdge->next=NULL;
+    newEdge->start->route=NULL;
+    newEdge->start->next=NULL;
+    newEdge->stop->route=NULL;
+    newEdge->stop->next=NULL;
+    newStop->next=NULL;
+    newStop->route=NULL;
+    InsertStop(&Stops, &newStop);
+    InsertEdge(&Edges, &newEdge);
+  }  
+puts("Database loaded!\n");
+
+  int choice = -1;
+  while(choice != 0){
+    puts("Please choose an option!\n");
+    puts("1. Print Stations");    
+    puts("2. Get shortest route");
+    puts("--Travel--");   
+    puts("3. Arrival time ");
+    puts("4. Depature time");
+    puts("0. Exit MENU");
+    printf("? ");
+    scanf("%d", &choice);
+    while(getchar() != '\n'); // Clear stdin
+    switch(choice){
+    case 1:
+      printStops(Stops);
+    }
+   
   }
-
-  }
-*/
+ return 0;
+}
